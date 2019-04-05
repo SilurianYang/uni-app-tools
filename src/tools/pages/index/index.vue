1eb058fd86e85ba0a8f2fbe3caa72554f0a61d75
hhyang <1606726660@qq.com>
2019-04-05 12:19:47 +0800

    Submit the basic architecture

---
+++ src/tools/pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="content">
+		<view>
+        </view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		text-align: center;
+		height: 400upx;
+	}
+    .logo{
+        height: 200upx;
+        width: 200upx;
+        margin-top: 200upx;
+    }
+	.title {
+		font-size: 36upx;
+		color: #8f8f94;
+	}
+</style>
