40589f7be7bceb603ddf118275b3d7ee0a166b7f
hhyang <1606726660@qq.com>
2019-04-06 21:35:13 +0800

    Add test upload file method

--- src/tools/pages/index/index.vue
+++ src/tools/pages/index/index.vue
@@ -2,6 +2,7 @@
 	<view class="content">
 		<view>
 			<button type="primary" @click="testReq">测试ajax</button>
+			<button type="primary" @click="testUp">测试上传文件</button>
 		</view>
 	</view>
 </template>
@@ -14,22 +15,34 @@
 			}
 		},
 		onLoad() {
-
+			this.$ToolsUp.test();
 		},
 		methods: {
+		async	testUp() {
+			const res=await	this.$ToolsUp.selectFiles({
+					type:0,
+					maximum:3,
+// 					upload:{
+// 						path:'example/upload',
+// 						files:['image','image','image'],
+// 						isUp:true
+// 					}
+				})
+				console.log(JSON.stringify(res))
+
+
+			},
 			async testReq() {
 				try {
 					const res = await this.$req.ajax({
 						path: "example/query",
-						title:"正在加载",
+						title: "正在加载",
 						data: {
 							token: '000-000-000-000-player125',
 							tpName: 'TP427613'
 						},
-						abortFun: bt => {
-						},
-						finshFun: finsh => {
-						}
+						abortFun: bt => {},
+						finshFun: finsh => {}
 					}, {
 						name: 66,
 						age: 99
