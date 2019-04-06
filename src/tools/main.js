bc972c6d9c0b56704b7c56ada7086b7bd94cc3aa
hhyang <1606726660@qq.com>
2019-04-06 21:38:38 +0800

    Help documentation entry file changes

--- src/tools/main.js
+++ src/tools/main.js
@@ -3,12 +3,14 @@ import App from './App'
 
 Vue.config.productionTip = false
 
-import req from './common/request/request.js';
+import {req} from './common/request/request.js';
+import  ToolsUp from './common/request/request-upFiles.js';
 
-req.defaultReq.url = 'https://www.easy-mock.com/mock/5ca6ec41215a7b66ff10343d/'
-
-req.defaultReq.type = "post";
+req.baseuUrl = 'https://www.easy-mock.com/mock/5ca6ec41215a7b66ff10343d/'
+req.defaultReq.type = "POST";
 Vue.prototype.$req = req;
+Vue.prototype.$ToolsUp = ToolsUp;
+
 
 App.mpType = 'app'
 
