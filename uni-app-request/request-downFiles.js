55bcd1cdfec29e1188825bcae88d33c8b5abe14e
hhyang <1606726660@qq.com>
2019-04-09 14:09:02 +0800

    Remove the basic download method

--- uni-app-request/request-downFiles.js
+++ uni-app-request/request-downFiles.js
@@ -9,30 +9,6 @@ class DonwFiles extends RQ {
 		super(arg);
 	}
 	/**
-	 * 内部下载文件，仅内部调用
-	 */
-	_downFiles(extra) {
-		return new Promise((resolve, reject) => {
-			const downloadTask = uni.downloadFile({
-				...extra,
-				complete: ({
-					statusCode = 0,
-					...finsh
-				} = {}) => {
-					extra.abort(downloadTask, Object.assign({}, {
-						statusCode,
-						...finsh
-					}));
-					if (statusCode === 200) {
-						return resolve(finsh);
-					}
-					return reject(finsh)
-				},
-			})
-			extra.abort(downloadTask);
-		})
-	}
-	/**
 	 * 下载文件
 	 */
 	startDownFiles({
@@ -56,7 +32,7 @@ class DonwFiles extends RQ {
 				path = path.toString().split(',');
 				for (let i = 0; i < path.length; i++) {
 					let url = path[i];
-					let res = await this._downFiles({
+					let res = await this.downFiles({
 						url,
 						abort,
 						...extra
