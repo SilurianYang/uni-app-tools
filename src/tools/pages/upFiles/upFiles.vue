05ad58626d68d60dd1c7d1a9e904c6c9fe31621f
hhyang <1606726660@qq.com>
2019-07-05 18:49:39 +0800

    Test project for selectFiles, AppSelectFiles methods

--- src/tools/pages/upFiles/upFiles.vue
+++ src/tools/pages/upFiles/upFiles.vue
@@ -4,7 +4,12 @@
 		<!-- #ifdef APP-PLUS -->
 		<button type="primary" @click="testUp(0)">选择并上图片</button>
 		<button type="primary" @click="testUp(1)">选择并上视频</button>
+
+		<button type="warn" @click="AppSelectFiles">app选择文件</button>
+
 		<!-- #endif -->
+		<button type="warn" @click="otherSelectFiles">H5选择文件</button>
+		
 		<button type="primary" @click="upnNetRes">上传网络文件</button>
 	</view>
 </template>
@@ -17,6 +22,20 @@
 			};
 		},
 		methods: {
+			async otherSelectFiles(){
+				const res = await this.$rup.otherSelectFiles({
+					maximum: 3,
+				})
+				console.log(res);
+			},
+			async AppSelectFiles() {
+				const res = await this.$rup.AppSelectFiles({
+					type: 0,
+					maximum: 3,
+					multiple: true
+				})
+				console.log(res);
+			},
 			async testUp(type) {
 				try {
 					const res = await this.$rup.selectFiles({
@@ -26,18 +45,18 @@
 							path: 'example/upload',
 							files: ['image'],
 							title: '正在上传',
-							abort:(info,bt)=>{
-								if(info.args.index==0){
+							abort: (info, bt) => {
+								if (info.args.index == 0) {
 									bt.abort();
 								}
 							},
-							extra:{
-								name:'我是你爸爸',
-								ages:21
+							extra: {
+								name: '我是你爸爸',
+								ages: 21
 							},
-							username:'hhyang'
+							username: 'hhyang'
 						},
-						
+
 					})
 					console.log(res)
 				} catch (e) {
@@ -47,25 +66,28 @@
 			/**
 			 * 上传网络文件
 			 */
-			async upnNetRes(){
-				this.$rup.defaultFile.upOpenDown=true;
-				let res=await this.$rup.upnNetRes({
-						netPath:['https://img.alicdn.com/imgextra/i3/117331861/TB24K.sayMnBKNjSZFCXXX0KFXa_!!0-saturn_solar.jpg_180x180.jpg','https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=421118981,56990953&fm=27&gp=0.jpg'],
-						upPath:'example/upload',
-						abort:(info,bt)=>{
-							// if(info.args.index==0){
-							// 	bt.abort();
-							// }
-							// console.log(info)
-						},
-						files: ['image'],
-						title:'正在上传',
-						extra:{
-							name:'我是你爸爸'
-						},
-						username:'hhyang'
-					})
-					console.log(res)
+			async upnNetRes() {
+				this.$rup.defaultFile.upOpenDown = true;
+				let res = await this.$rup.upnNetRes({
+					netPath: [
+						'https://img.alicdn.com/imgextra/i3/117331861/TB24K.sayMnBKNjSZFCXXX0KFXa_!!0-saturn_solar.jpg_180x180.jpg',
+						'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=421118981,56990953&fm=27&gp=0.jpg'
+					],
+					upPath: 'example/upload',
+					abort: (info, bt) => {
+						// if(info.args.index==0){
+						// 	bt.abort();
+						// }
+						// console.log(info)
+					},
+					files: ['image'],
+					title: '正在上传',
+					extra: {
+						name: '我是你爸爸'
+					},
+					username: 'hhyang'
+				})
+				console.log(res)
 			}
 		},
 	}
